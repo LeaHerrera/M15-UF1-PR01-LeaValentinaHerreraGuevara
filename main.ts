145300c6b9b4f110af550ab64c5f6eaf371ff8b4
LeaHerrera <146827129+LeaHerrera@users.noreply.github.com>
2026-04-15 23:19:48 +0200

    comit iniciar de bases

--- main.ts
+++ main.ts
@@ -1,1 +1,28 @@
- 
+namespace SpriteKind {
+    export const coin = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (myCatSprite.vy == 0) {
+        myCatSprite.vy = -125
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.coin, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    sprites.destroy(otherSprite, effects.rings, 500)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`miMosaico`, function (sprite, location) {
+    game.gameOver(false)
+})
+let coin: Sprite = null
+let myCatSprite: Sprite = null
+scene.setBackgroundColor(13)
+myCatSprite = sprites.create(assets.image`theCat`, SpriteKind.Player)
+controller.moveSprite(myCatSprite, 100, 0)
+tiles.setCurrentTilemap(tilemap`nivel1`)
+myCatSprite.ay = 350
+scene.cameraFollowSprite(myCatSprite)
+for (let valor of tiles.getTilesByType(assets.tile`money`)) {
+    coin = sprites.create(assets.image`coin`, SpriteKind.coin)
+    tiles.placeOnTile(coin, valor)
+    tiles.setTileAt(valor, assets.tile`transparency16`)
+}
